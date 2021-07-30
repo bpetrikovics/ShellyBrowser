6300ae9bee40f05b68aa059732bbad249e742b6f
Balazs Petrikovics <bpetrikovics@gmail.com>
2021-07-30 17:13:49 +0200

    ShellyFirmwareApi: Deserialize firmware API data, add methods to find latest version for model

--- Shelly-OTA-Win/ShellyFirmwareAPI.cs
+++ Shelly-OTA-Win/ShellyFirmwareAPI.cs
@@ -1,7 +1,9 @@
 ﻿//using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 using System.Net.Http;
 
-using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 // https://stackoverflow.com/questions/16339167/how-do-i-deserialize-a-complex-json-object-in-c-sharp-net
 
@@ -27,15 +29,55 @@ namespace Shelly_OTA_Win
     {
         private static readonly HttpClient client = new HttpClient();
         private static readonly string Baseurl = "https://api.shelly.cloud/files/firmware";
+        private static List<ShellyFirmwareVersion> fwdata = new();
 
         // TODO: properly parse and interpret shelly API response
         public static async void Init()
         {
-            var result = await client.GetStringAsync(Baseurl);
-            dynamic jsondata = JsonConvert.DeserializeObject(result);
-            var fwlist = jsondata.data;
+            var json = await client.GetStringAsync(Baseurl);
+            JObject data = JObject.Parse(json);
+            
+            // Check if API reports the data is healthy
+            if ((bool)(data["isok"]) is false)
+            {
+                throw new InvalidOperationException();
+            }
+
+            foreach (var (key, value) in data["data"].ToObject<Dictionary<string, ShellyFirmwareVersion>>())
+            {
+                value.deviceModel = key;
+                fwdata.Add(value);
+            }
+        }
+
+        public static string getLatestVersionForModel(string model)
+        {
+            return fwdata.Find(x => x.deviceModel == model).availableVersion;
+        }
+
+        public static ShellyFirmwareVersion getLatestFirmware(ShellyDevice device)
+        {
+            return fwdata.Find(x => x.deviceModel == device.type);
         }
     }
 
- 
+
+    class ShellyFirmwareVersion
+    {
+        public string deviceModel { get; set; }
+        public string availableVersion { get; set; }
+        public string downloadUrl { get; set; }
+
+        public ShellyFirmwareVersion(string model, string version, string url)
+        {
+            this.deviceModel = model;
+            this.availableVersion = version;
+            this.downloadUrl = url;
+        }
+
+        public override string ToString()
+        {
+            return $"<ShellyFirmware model={this.deviceModel}, version={this.availableVersion}, url={this.downloadUrl}>";
+        }
+    }
 }
