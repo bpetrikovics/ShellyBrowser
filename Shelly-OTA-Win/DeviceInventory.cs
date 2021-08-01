2981503ad75d9fa7f0c628481560536c3f5ec3d7
Balazs Petrikovics <bpetrikovics@gmail.com>
2021-08-01 16:37:04 +0200

    clean 'using's

--- Shelly-OTA-Win/DeviceInventory.cs
+++ Shelly-OTA-Win/DeviceInventory.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Shelly_OTA_Win
 {
