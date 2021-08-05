8dac063b23918e3112ac30f8db9ddeeba0384cb9
Balazs Petrikovics <bpetrikovics@gmail.com>
2021-08-05 12:25:00 +0200

    Moving device discovery into the inventory class

--- Shelly-OTA-Win/DeviceInventory.cs
+++ Shelly-OTA-Win/DeviceInventory.cs
@@ -1,20 +1,93 @@
-﻿using System.Collections.Generic;
+﻿using Makaretu.Dns;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Shelly_OTA_Win
 {
     class DeviceInventory
     {
+        private static readonly MulticastService mdns = new();
+        private static readonly ServiceDiscovery sd = new(mdns);
+
         private PresentationService presenter;
         private StatusService status_service;
 
         private List<ShellyDevice> devices;
+
+        private static System.Threading.Timer AgeCheckTimer;
+
+        // to be moved to app.config property later
+        public static readonly int maxDeviceAge = 30;
 
         public DeviceInventory(PresentationService pservice, StatusService sservice)
         {
             presenter = pservice;
             status_service = sservice;
             devices = new();
+
+            AgeCheckTimer = new System.Threading.Timer(new System.Threading.TimerCallback(DeviceAgeCheck), null, 1250, 500);
+
+            mdns.AnswerReceived += onMdnsAnswerReceived;
+            mdns.Start();
+            mdns.SendQuery("_http._tcp.local");
+        }
+
+        private void DeviceAgeCheck(object state)
+        {
+            var state_changed = false;
+
+            foreach (var device in All())
+            {
+                if ((device.Age() >= maxDeviceAge) && (device.stale is false))
+                {
+                    // Transition available -> stale
+                    device.stale = true;
+                    state_changed = true;
+                }
+                else if ((device.Age() < maxDeviceAge) && (device.stale is true))
+                {
+                    status_service.Update($"Device {device.name} no longer stale");
+                    device.stale = false;
+                    state_changed = true;
+                }
+            }
+
+            if (state_changed)
+            {
+                presenter.RefreshListView(All());
+            }
+        }
+
+        // As this is being called from the mdns service, it may be run in a thread which is separate
+        // from the thread where the Form exists. Therefore the form controls need to be accessed in a
+        // thread-safe way via delegate function
+        private async void onMdnsAnswerReceived(object sender, MessageEventArgs e)
+        {
+            var addresses = e.Message.Answers.OfType<AddressRecord>();
+
+            foreach (var address in addresses)
+            {
+                if (address.Name.ToString().StartsWith("shelly"))
+                {
+                    ShellyDevice mydev = FindByName(address.Name.ToString());
+                    if (mydev is not null)
+                    {
+                        status_service.Update($"Received update for device: {mydev.name}");
+                        mydev.UpdateLastSeen();
+                    }
+                    else
+                    {
+                        status_service.Update($"Discovering device: {address.Name} at {address.Address}");
+                        mydev = await ShellyDevice.Discover(address);
+
+                        status_service.Update($"Discovered new {mydev.type} device at {mydev.address}");
+                        AddDevice(mydev);
+                        presenter.RefreshListView(All());
+
+                        status_service.UpdateDeviceCount(Count);
+                    }
+                }
+            }
         }
 
         public void AddDevice(ShellyDevice device)
@@ -44,5 +117,12 @@ namespace Shelly_OTA_Win
                 return devices.Count;
             }
         }
+
+        public void Dispose()
+        {
+            sd.Dispose();
+            mdns.Stop();
+        }
+
     }
 }
--- Shelly-OTA-Win/OTABrowser.cs
+++ Shelly-OTA-Win/OTABrowser.cs
@@ -13,21 +13,11 @@ namespace Shelly_OTA_Win
 {
     public partial class OTABrowser : Form
     {
-        private static readonly MulticastService mdns = new();
-        private static readonly ServiceDiscovery sd = new(mdns);
-
         private DeviceInventory inventory;
         private PresentationService presenter;
         private StatusService status;
 
-        // This needs to be a class variable otherwise if just defined as a local variable, the garbage collector
-        // will collect it due to lack of reference
-        private static System.Threading.Timer AgeCheckTimer;
-
         internal static List<ShellyDevice> Devices = new();
-
-        // to be moved to app.config property later
-        public static readonly int maxDeviceAge = 30;
 
         public OTABrowser()
         {
@@ -37,19 +27,12 @@ namespace Shelly_OTA_Win
             status = new StatusService(StatusStrip);
             presenter = new PresentationService(DeviceListView);
             inventory = new DeviceInventory(presenter, status);
+            ShellyFirmwareAPI.Init();
         }
 
         private void onMainFormLoad(object sender, EventArgs e)
         {
-            status.Update("Checking latest firmware versions");
-            ShellyFirmwareAPI.Init();
-
-            AgeCheckTimer = new System.Threading.Timer(new System.Threading.TimerCallback(DeviceAgeCheck), null, 1250, 500);
-
             status.Update("Please wait, devices will appear in the list once detected...");
-            mdns.AnswerReceived += onMdnsAnswerReceived;
-            mdns.Start();
-            mdns.SendQuery("_http._tcp.local");
         }
 
         // This doesn't seem to properly work, likely because of threads e.g.
@@ -57,66 +40,7 @@ namespace Shelly_OTA_Win
         // Exception thrown: 'System.ObjectDisposedException' in System.Private.CoreLib.dll
         private void onMainFormClosing(object sender, EventArgs e)
         {
-            sd.Dispose();
-            mdns.Stop();
-        }
-
-        private void DeviceAgeCheck(object state)
-        {
-            var state_changed = false;
-
-            foreach (var device in inventory.All())
-            {
-                if ((device.Age() >= maxDeviceAge) && (device.stale is false))
-                {
-                    // Transition available -> stale
-                    device.stale = true;
-                    state_changed = true;
-                }
-                else if ((device.Age() < maxDeviceAge) && (device.stale is true))
-                {
-                    status.Update($"Device {device.name} no longer stale");
-                    device.stale = false;
-                    state_changed = true;
-                }
-            }
-
-            if(state_changed)
-            {
-                presenter.RefreshListView(inventory.All());
-            }
-        }
-
-        // As this is being called from the mdns service, it may be run in a thread which is separate
-        // from the thread where the Form exists. Therefore the form controls need to be accessed in a
-        // thread-safe way via delegate function
-        private async void onMdnsAnswerReceived(object sender, MessageEventArgs e)
-        {
-            var addresses = e.Message.Answers.OfType<AddressRecord>();
-
-            foreach (var address in addresses)
-            {
-                if (address.Name.ToString().StartsWith("shelly"))
-                {
-                    ShellyDevice mydev = inventory.FindByName(address.Name.ToString());
-                    if (mydev is not null)
-                    {
-                        status.Update($"Received update for device: {mydev.name}");
-                        mydev.UpdateLastSeen();
-                    }
-                    else
-                    {
-                        status.Update($"Discovering device: {address.Name} at {address.Address}");
-                        mydev = await ShellyDevice.Discover(address);
-
-                        status.Update($"Discovered new {mydev.type} device at {mydev.address}");
-                        inventory.AddDevice(mydev);
-                        presenter.RefreshListView(inventory.All());
-
-                        status.UpdateDeviceCount(inventory.Count);
-                    }
-                }
-            }
+            inventory.Dispose();
         }
 
         private void DeviceListView_SelectedIndexChanged(object sender, EventArgs e)
